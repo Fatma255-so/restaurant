25327d543e68e56f787605ac519239f6e7fce048
fatma abdalla <abdallafatma66@gmail.com>
2022-05-19 15:12:15 +0200

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SliderComponent } from './slider/slider.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { MenuComponent } from './menu/menu.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SliderComponent,
+    AboutComponent,
+    BlogComponent,
+    MenuComponent,
+    ContactusComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
